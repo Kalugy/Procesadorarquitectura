f68c2934f30d5e4f55d1ae3be37933bd561b4be9
Kalugy <danieltrujillo901@gmail.com>
2017-11-09 23:27:27 -0500

    decodeee

--- Segmentado/SEU_30.vhd
+++ Segmentado/SEU_30.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date:    15:13:56 09/26/2017 
+-- Create Date:    10:14:04 10/20/2017 
 -- Design Name: 
--- Module Name:    PC - arqPC 
+-- Module Name:    SEU_30 - Behavioral 
 -- Project Name: 
 -- Target Devices: 
 -- Tool versions: 
@@ -19,7 +19,7 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.std_logic_unsigned.all;
+
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
@@ -29,25 +29,19 @@ use IEEE.std_logic_unsigned.all;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity NPC is
-    Port ( inNPC : in  STD_LOGIC_VECTOR (31 downto 0);
-           Reset : in  STD_LOGIC;
-           Clk : in  STD_LOGIC;
-           outNPC : out  STD_LOGIC_VECTOR (31 downto 0));
-end NPC;
+entity SEU_30 is
+    Port ( Imm_30 : in  STD_LOGIC_VECTOR (29 downto 0);
+           Imm_32 : out  STD_LOGIC_VECTOR (31 downto 0));
+end SEU_30;
 
-architecture arqNPC of NPC is
-
+architecture Behavioral of SEU_30 is
 begin
-	process(Clk,Reset,inNPC)
-	begin
-		if(Reset = '1')then
-			outNPC<="00000000000000000000000000000000";	
-		else 
-			if(rising_edge(Clk)) then
-				outNPC<=inNPC;
-			end if;
+	process (Imm_30 ) begin
+		if(Imm_30(29) = '1')then
+			Imm_32<= "11" & Imm_30;
+		elsif(Imm_30(29) = '0')then
+			Imm_32<= "00" & Imm_30;
 		end if;
-	end process;
-end arqNPC;
+	end process;	
 
+end Behavioral;
