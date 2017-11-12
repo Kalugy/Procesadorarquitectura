20b349a8bab2bb5c48994fe22795bd8017e24ace
Kalugy <danieltrujillo901@gmail.com>
2017-11-11 21:47:14 -0500

    cx

---
+++ Segmentado/Union.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    21:44:52 11/11/2017 
+-- Design Name: 
+-- Module Name:    Union - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Union is
+    Port ( Clk : in  STD_LOGIC;
+           reset : in  STD_LOGIC);
+end Union;
+
+architecture Behavioral of Union is
+
+begin
+
+
+end Behavioral;
+
